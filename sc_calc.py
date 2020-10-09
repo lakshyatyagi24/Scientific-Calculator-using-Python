f5175decff7858a0eddcc30dd859656bebf01aaa
GSantos23 <santosgerson64@gmail.com>
2020-10-08 21:30:19 -0600

    Fix Issue #1
    Typo from Standadr -> Standard on File menu
    Change colors to emulate Ti-84 Plus CE
    Fix Scientific Mode width

--- sc_calc.py
+++ sc_calc.py
@@ -237,61 +237,61 @@ i=0
 btn=[]
 for j in range(2,5):
     for k in range(3):
-        btn.append(Button(calc, width=6, height=2, font=('arial', 20, 'bold'), bd=4, text=numberpad[i]))
+        btn.append(Button(calc, width=6, height=2, font=('arial', 20, 'bold'), bd=4, bg="gray99",text=numberpad[i]))
         btn[i].grid(row=j, column=k, pady=1)
         btn[i]["command"]=lambda x=numberpad[i]: added_value.numberEnter(x)
         i+=1
 
 #========================Standard Function==============================================================================================================================================
 
-btnClear=Button(calc, text=chr(67), width=6, height=2, font=('arial', 20, 'bold'), bd=4, bg="powder blue", command=added_value.Clear_Entry).grid(row=1, column=0, pady=1)
-btnAllClear=Button(calc, text=chr(67)+chr(69), width=6, height=2, font=('arial', 20, 'bold'), bd=4, bg="powder blue", command=added_value.all_Clear_Entry).grid(row=1, column=1, pady=1)
+btnClear=Button(calc, text=chr(67), width=6, height=2, font=('arial', 20, 'bold'), bd=4, fg="gray99", bg="gray5", command=added_value.Clear_Entry).grid(row=1, column=0, pady=1)
+btnAllClear=Button(calc, text=chr(67)+chr(69), width=6, height=2, font=('arial', 20, 'bold'), bd=4, fg="gray99", bg="gray5", command=added_value.all_Clear_Entry).grid(row=1, column=1, pady=1)
 
-btnSq=Button(calc, text="√", width=6, height=2, font=('arial', 20, 'bold'), bd=4, bg="powder blue", command=added_value.squared).grid(row=1, column=2, pady=1)
-btnAdd=Button(calc, text="+", width=6, height=2, font=('arial', 20, 'bold'), bd=4, bg="powder blue", command=lambda:added_value.operation("add")).grid(row=1, column=3, pady=1)
+btnSq=Button(calc, text="√", width=6, height=2, font=('arial', 20, 'bold'), bd=4, fg="gray99", bg="gray5", command=added_value.squared).grid(row=1, column=2, pady=1)
+btnAdd=Button(calc, text="+", width=6, height=2, font=('arial', 20, 'bold'), bd=4, bg="gray99", command=lambda:added_value.operation("add")).grid(row=1, column=3, pady=1)
 
-btnSub=Button(calc, text="-", width=6, height=2, font=('arial', 20, 'bold'), bd=4, bg="powder blue", command=lambda:added_value.operation("sub")).grid(row=2, column=3, pady=1)
-btnMult=Button(calc, text="×", width=6, height=2, font=('arial', 20, 'bold'), bd=4, bg="powder blue", command=lambda:added_value.operation("multi")).grid(row=3, column=3, pady=1)
+btnSub=Button(calc, text="-", width=6, height=2, font=('arial', 20, 'bold'), bd=4, bg="gray99", command=lambda:added_value.operation("sub")).grid(row=2, column=3, pady=1)
+btnMult=Button(calc, text="×", width=6, height=2, font=('arial', 20, 'bold'), bd=4, bg="gray99", command=lambda:added_value.operation("multi")).grid(row=3, column=3, pady=1)
 
-btnDiv=Button(calc, text=chr(247), width=6, height=2, font=('arial', 20, 'bold'), bd=4, bg="powder blue", command=lambda:added_value.operation("divide")).grid(row=4, column=3, pady=1)
-btnZero=Button(calc, text="0", width=6, height=2, font=('arial', 20, 'bold'), bd=4, bg="powder blue", command=lambda:added_value.numberEnter(0)).grid(row=5, column=0, pady=1)
+btnDiv=Button(calc, text=chr(247), width=6, height=2, font=('arial', 20, 'bold'), bd=4, bg="gray99", command=lambda:added_value.operation("divide")).grid(row=4, column=3, pady=1)
+btnZero=Button(calc, text="0", width=6, height=2, font=('arial', 20, 'bold'), bd=4, bg="gray99", command=lambda:added_value.numberEnter(0)).grid(row=5, column=0, pady=1)
 
-btnDot=Button(calc, text=".", width=6, height=2, font=('arial', 20, 'bold'), bd=4, bg="powder blue", command=lambda:added_value.numberEnter(".")).grid(row=5, column=1, pady=1)
-btnPM=Button(calc, text=chr(177), width=6, height=2, font=('arial', 20, 'bold'), bd=4, bg="powder blue", command=added_value.mathsPM).grid(row=5, column=2, pady=1)
+btnDot=Button(calc, text=".", width=6, height=2, font=('arial', 20, 'bold'), bd=4, bg="gray99", command=lambda:added_value.numberEnter(".")).grid(row=5, column=1, pady=1)
+btnPM=Button(calc, text=chr(177), width=6, height=2, font=('arial', 20, 'bold'), bd=4, bg="gray99", command=added_value.mathsPM).grid(row=5, column=2, pady=1)
 
-btnEquals=Button(calc, text="=", width=6, height=2, font=('arial', 20, 'bold'), bd=4, bg="powder blue", command=added_value.sum_of_total).grid(row=5, column=3, pady=1)
+btnEquals=Button(calc, text="=", width=6, height=2, font=('arial', 20, 'bold'), bd=4, bg="gray99", command=added_value.sum_of_total).grid(row=5, column=3, pady=1)
 
 #===================Scientific Calculator====================================================================================================================================================
 
-btnPi=Button(calc, text='π', width=6, height=2, font=('arial', 20, 'bold'), bd=4, bg="Aqua", command=added_value.pi).grid(row=1, column=4, pady=1)
-btnCos=Button(calc, text="cos", width=6, height=2, font=('arial', 20, 'bold'), bd=4, bg="Aqua", command=added_value.cos).grid(row=1, column=5, pady=1)
+btnPi=Button(calc, text='π', width=6, height=2, font=('arial', 20, 'bold'), bd=4, fg="gray99", bg="gray5", command=added_value.pi).grid(row=1, column=4, pady=1)
+btnCos=Button(calc, text="cos", width=6, height=2, font=('arial', 20, 'bold'), bd=4, fg="gray99", bg="gray5", command=added_value.cos).grid(row=1, column=5, pady=1)
 
-btnTan=Button(calc, text="tan", width=6, height=2, font=('arial', 20, 'bold'), bd=4, bg="Aqua", command=added_value.tan).grid(row=1, column=6, pady=1)
-btnSin=Button(calc, text="sin", width=6, height=2, font=('arial', 20, 'bold'), bd=4, bg="Aqua", command=added_value.sin).grid(row=1, column=7, pady=1)
+btnTan=Button(calc, text="tan", width=6, height=2, font=('arial', 20, 'bold'), bd=4, fg="gray99", bg="gray5", command=added_value.tan).grid(row=1, column=6, pady=1)
+btnSin=Button(calc, text="sin", width=6, height=2, font=('arial', 20, 'bold'), bd=4, fg="gray99", bg="gray5", command=added_value.sin).grid(row=1, column=7, pady=1)
 
-btn2Pi=Button(calc, text='2π', width=6, height=2, font=('arial', 20, 'bold'), bd=4, bg="Aqua", command=added_value.tau).grid(row=2, column=4, pady=1)
-btnCosh=Button(calc, text="cosh", width=6, height=2, font=('arial', 20, 'bold'), bd=4, bg="gray", command=added_value.cosh).grid(row=2, column=5, pady=1)
+btn2Pi=Button(calc, text='2π', width=6, height=2, font=('arial', 20, 'bold'), bd=4, fg="gray99", bg="gray5", command=added_value.tau).grid(row=2, column=4, pady=1)
+btnCosh=Button(calc, text="cosh", width=6, height=2, font=('arial', 20, 'bold'), bd=4, fg="gray99", bg="gray5", command=added_value.cosh).grid(row=2, column=5, pady=1)
 
-btnTanh=Button(calc, text="tanh", width=6, height=2, font=('arial', 20, 'bold'), bd=4, bg="gray", command=added_value.tanh).grid(row=2, column=6, pady=1)
-btnSinh=Button(calc, text="sinh", width=6, height=2, font=('arial', 20, 'bold'), bd=4, bg="gray", command=added_value.sinh).grid(row=2, column=7, pady=1)
+btnTanh=Button(calc, text="tanh", width=6, height=2, font=('arial', 20, 'bold'), bd=4, fg="gray99", bg="gray5", command=added_value.tanh).grid(row=2, column=6, pady=1)
+btnSinh=Button(calc, text="sinh", width=6, height=2, font=('arial', 20, 'bold'), bd=4, fg="gray99", bg="gray5", command=added_value.sinh).grid(row=2, column=7, pady=1)
 
-btnLog=Button(calc, text='log', width=6, height=2, font=('arial', 20, 'bold'), bd=4, bg="Aqua", command=added_value.log).grid(row=3, column=4, pady=1)
-btninv=Button(calc, text="Inv", width=6, height=2, font=('arial', 20, 'bold'), bd=4, bg="gray", command=lambda:added_value.operation("inv")).grid(row=3, column=5, pady=1)
+btnLog=Button(calc, text='log', width=6, height=2, font=('arial', 20, 'bold'), bd=4, fg="gray99", bg="gray5", command=added_value.log).grid(row=3, column=4, pady=1)
+btninv=Button(calc, text="Inv", width=6, height=2, font=('arial', 20, 'bold'), bd=4, fg="gray99", bg="gray5", command=lambda:added_value.operation("inv")).grid(row=3, column=5, pady=1)
 
-btnMod=Button(calc, text="Mod", width=6, height=2, font=('arial', 20, 'bold'), bd=4, command=lambda:added_value.operation("mod")).grid(row=3, column=6, pady=1)
-btnE=Button(calc, text="e", width=6, height=2, font=('arial', 20, 'bold'), bd=4, bg="gray", command=added_value.e).grid(row=3, column=7, pady=1)
+btnMod=Button(calc, text="Mod", width=6, height=2, font=('arial', 20, 'bold'), bd=4, fg="gray99", bg="lime green", command=lambda:added_value.operation("mod")).grid(row=3, column=6, pady=1)
+btnE=Button(calc, text="e", width=6, height=2, font=('arial', 20, 'bold'), bd=4, fg="gray99", bg="gray5", command=added_value.e).grid(row=3, column=7, pady=1)
 
-btnLog2=Button(calc, text='log2', width=6, height=2, font=('arial', 20, 'bold'), bd=4, bg="Aqua", command=added_value.log2).grid(row=4, column=4, pady=1)
-btnDeg=Button(calc, text="deg", width=6, height=2, font=('arial', 20, 'bold'), bd=4, bg="gray", command=added_value.degrees).grid(row=4, column=5, pady=1)
+btnLog2=Button(calc, text='log2', width=6, height=2, font=('arial', 20, 'bold'), bd=4, fg="gray99", bg="gray5", command=added_value.log2).grid(row=4, column=4, pady=1)
+btnDeg=Button(calc, text="deg", width=6, height=2, font=('arial', 20, 'bold'), bd=4, fg="gray99", bg="gray5", command=added_value.degrees).grid(row=4, column=5, pady=1)
 
-btnAcosh=Button(calc, text="acosh", width=6, height=2, font=('arial', 20, 'bold'), bd=4, bg="gray", command=added_value.acosh).grid(row=4, column=6, pady=1)
-btnAsinh=Button(calc, text="asinh", width=6, height=2, font=('arial', 20, 'bold'), bd=4, bg="gray", command=added_value.asinh).grid(row=4, column=7, pady=1)
+btnAcosh=Button(calc, text="acosh", width=6, height=2, font=('arial', 20, 'bold'), bd=4, fg="gray99", bg="gray5", command=added_value.acosh).grid(row=4, column=6, pady=1)
+btnAsinh=Button(calc, text="asinh", width=6, height=2, font=('arial', 20, 'bold'), bd=4, fg="gray99", bg="gray5", command=added_value.asinh).grid(row=4, column=7, pady=1)
 
-btnLog10=Button(calc, text='log10', width=6, height=2, font=('arial', 20, 'bold'), bd=4, bg="Aqua", command=added_value.log10).grid(row=5, column=4, pady=1)
-btnLog1p=Button(calc, text="log1p", width=6, height=2, font=('arial', 20, 'bold'), bd=4, bg="Aqua", command=added_value.log1p).grid(row=5, column=5, pady=1)
+btnLog10=Button(calc, text='log10', width=6, height=2, font=('arial', 20, 'bold'), bd=4, fg="gray99", bg="gray5", command=added_value.log10).grid(row=5, column=4, pady=1)
+btnLog1p=Button(calc, text="log1p", width=6, height=2, font=('arial', 20, 'bold'), bd=4, fg="gray99", bg="gray5", command=added_value.log1p).grid(row=5, column=5, pady=1)
 
-btnExpm1=Button(calc, text="expm1", width=6, height=2, font=('arial', 20, 'bold'), bd=4, bg="Aqua", command=added_value.expm1).grid(row=5, column=6, pady=1)
-btnLgamma=Button(calc, text="lgamma", width=6, height=2, font=('arial', 20, 'bold'), bd=4, bg="Aqua", command=added_value.lgamma).grid(row=5, column=7, pady=1)
+btnExpm1=Button(calc, text="expm1", width=6, height=2, font=('arial', 20, 'bold'), bd=4, fg="gray99", bg="gray5", command=added_value.expm1).grid(row=5, column=6, pady=1)
+btnLgamma=Button(calc, text="lgamma", width=6, height=2, font=('arial', 20, 'bold'), bd=4, fg="gray99", bg="gray5", command=added_value.lgamma).grid(row=5, column=7, pady=1)
 
 #===============================Display Text======================================================================================================================================
 
@@ -313,10 +313,10 @@ def iExit():
 
 def Scientific():
     root.resizable(width=False, height=False)
-    root.geometry("944x624+20+20")
+    root.geometry("990x624+20+20")
 
 def Standard():
-    root.resizable(width=False, height=False)
+    root.resizable(width=True, height=False)
     root.geometry("480x624+20+20")
 """
 def Volume():
@@ -372,9 +372,9 @@ def Angle():
 menubar = Menu(calc)
 
 
-filemenu = Menu(menubar, tearoff=0)
+filemenu = Menu(menubar, tearoff=0, relief=RAISED)
 menubar.add_cascade(label = "File", menu=filemenu)
-filemenu.add_command(label = "Standadrd", command = Standard)
+filemenu.add_command(label = "Standard", command = Standard)
 filemenu.add_command(label = "Scientific", command = Scientific)
 filemenu.add_separator()
 filemenu.add_command(label = "Exit", command = iExit)
